0c06914d262d47bde33e6968e6eba54439b0feb6
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2023-12-13 09:49:51 +0000

    Add content to scenarioThree and add clear() for cleaner look

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 import os
 import sys
-# import pyfiglet module 
+# import pyfiglet module
 import pyfiglet
 
-  
-result = pyfiglet.figlet_format("Skyrim  Word  Adventure  Game", font = "digital" ) 
-print(result + "A project created for code institute by Jennifer Hujanen\n\n")
+
+result = pyfiglet.figlet_format("Skyrim Word Adventure Game", font="digital")
+print(result+"A project created for code institute by Jennifer Hujanen\n\n")
+
 
 def prompt():
     """Display starting game message"""
-    print(
-        "Welcome to a funny word adventure game that will take place in Whiterun in the world of Skyrim"
-        )
-    
+    print("Welcome to a funny word adventure game that will"
+          "take place in Whiterun in the world of Skyrim")
+
     print("Hello Adventurer, what is your name?")
     player = input()
     print("\nWelcome\t" + player+"! Are your ready?\n")
@@ -21,65 +21,71 @@ def prompt():
 
 def clear():
     """
-    Clear out the welcome message and start the game when the user presses enter.
+    Clear out the welcome message and start
+    the game when the user presses enter.
     """
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 prompt()
 clear()
 
+
 def scenarioOne():
-    #clear()
 
     print("Scenario 1: The Peculiar Potion Peddler\n")
     print("While walking around in the town of Whiterun you encounter a peculiar alchemist selling bizarre potions. What do you do?\n")
     print("Option 1: Chug the Chuckle Elixir?\nOption 2: Negotiate for a Guffaw Grenade?\n")
-    
+
     userInput = input("Enter 1 or 2: ")
-    
+
     while userInput not in {"1", "2"}:
         print("Please enter 1 or 2")
         userInput = input("Enter 1 or 2: ")
-       
+    clear()
+
     if userInput == "1":
-        print("You entered option 1!\n")
+        print("You entered option 1!\n"
+              "Chug the Chuckle Elixir!")
         print(
-            "You drink a potion labeled 'Chuckletonic', and suddenly everything becomes hilariously distorted.\n"
-            "NPCs start telling punchlines, and you navigate the town with uncontrollable giggles.\n"
+            "You drink a potion labeled 'Chuckletonic', 
+            "and suddenly everything becomes hilariously distorted.\n"
+            "NPCs start telling punchlines, 
+            "and you navigate the town with uncontrollable giggles.\n"
             "Hungry from all the laughter you head towards the tavern."
         )
-        
+
     elif userInput == "2":
         print("You entered option 2!\n")
         print(
-            "You haggle with the alchemist for a laughter-inducing explosive.\n" 
+            "You haggle with the alchemist for a laughter-inducing explosive.\n"
             "You accidentally toss it into a group of guards, who burst into fits of laughter, allowing you to sneak into the local tavern unnoticed."
         )
     input("Press enter to continue ...")
     clear()
-    
+
 
 def scenarioTwo():
-    #clear()
 
     print("Scenario 2: The Enchanted Lute\n")
     print("After that weird encounter with the peculiar alchemist you turn around a corner and stumble upon a magical lute. What do you do?\n")
     print("Option 1: Play the Chicken Serenade?\nOption 2: Fus Ro Jam?\n")
-    
+
     userInput = input("Enter 1 or 2: ")
-    
+
     while userInput not in {"1", "2"}:
         print("Please enter 1 or 2")
         userInput = input("Enter 1 or 2: ")
-       
+    clear()
+
     if userInput == "1":
         print("You entered option 1!\n")
         print(
-            "You strum a catchy tune dedicated to the town's chickens.\n" 
+            "You strum a catchy tune dedicated to the town's chickens.\n"
             "The fowl flock to you, forming a feathery entourage inside the small tavern.\n"
             "You gain the title 'Chicken chaser' and the chickens follow you as you leave Whiterun to search for your next adventure."
         )
-        
+
     elif userInput == "2":
         print("You entered option 2!\n")
         print(
@@ -90,28 +96,34 @@ def scenarioTwo():
             "You and your new companion walk out the gates of Whiterun to look for a new quests to conquer."
         )
     input("Press enter to continue ...")
+    clear()
+
 
 def scenarioThree():
-    #clear()
 
     print("Scenario 3: The Jolly Mammoth Ride\n")
-    print("After that weird encounter with the peculiar alchemist you turn around a corner and stumble upon a magical lute. What do you do?\n")
-    print("Option 1: Play the Chicken Serenade?\nOption 2: Fus Ro Jam?\n")
-    
+    print("After walking for a bit out in the plains of Whiterun, you spot what looks like a friendly giant herding mammoths. How do you approach this opportunity?\n")
+    print("Option 1: Join the Mammoth Parade?\nOption 2: Attempt a Mammoth Backflip?\n")
+
     userInput = input("Enter 1 or 2: ")
-    
+
     while userInput not in {"1", "2"}:
         print("Please enter 1 or 2")
         userInput = input("Enter 1 or 2: ")
-       
+    clear()
+
     if userInput == "1":
         print("You entered option 1!\n")
-        print(
-            "You strum a catchy tune dedicated to the town's chickens.\n" 
-            "The fowl flock to you, forming a feathery entourage inside the small tavern.\n"
-            "You gain the title 'Chicken chaser' and the chickens follow you as you leave Whiterun to search for your next adventure."
-        )
-        
+        print("You march alongside the giants, leading a mammoth parade through the plains and everyone's having a jolly good time.\n"
+              "Or so you think until you're surprised attacked by the giant swinging his club making you fly like the dragon you are inside.\n")
+        print("Too bad Dovakhiin isn't invicible, you flew straight to oblivion!\n")
+        restart = input("Press enter to restart the game ...")
+
+        if restart is True:
+            prompt()
+            clear()
+            
+
     elif userInput == "2":
         print("You entered option 2!\n")
         print(
@@ -122,8 +134,9 @@ def scenarioThree():
             "You and your new companion walk out the gates to Whiterun to look for a new quests to conquer"
         )
     input("Press enter to continue ...")
-    
-        
+    clear()
+
+
 scenarioOne()
 scenarioTwo()
 scenarioThree()
