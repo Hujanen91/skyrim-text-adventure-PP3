0dbad9f2b300b6777312dbb1bb50d5ee10bd14f0
Jennifer Hujanen <jennifer.hujanen@hotmail.com>
2023-12-15 06:35:11 +0000

    Add riddles, options and correct answer to riddles.py

--- riddles.py
+++ riddles.py
@@ -1,17 +1,34 @@
 RIDDLES = [
     {
-        "riddle": "What is the capital of France?",
-        "options": ["Berlin", "Madrid", "Paris", "Rome"],
-        "correct_answer": "Paris"
+        "riddle": "I'm a shout that echoes through the hills,\n"
+                  "Unleashing power that gives foes chills.\n"
+                  "Born of dragons, mighty and grand,\n"
+                  "What am I, in the land of Skyrim's command?\n",
+        "options": ["Dragon Shout", "Dragonborn", "Dragon Priest", "Daedric Artifact"],
+        "correct_answer": "Dragon Shout"
     },
     {
-        "riddle": "Which planet is known as the 'Red Planet'?",
-        "options": ["Mars", "Venus", "Jupiter", "Saturn"],
-        "correct_answer": "Mars"
+        "riddle": "In dungeons deep, where draugr sleep,\n"
+                  "A precious gem, its secrets keep.\n"
+                  "Lit by the mage's glowing hand,\n"
+                  "What am I, buried in the Nordic sand?\n",
+        "options": ["Dragon Claw", "Soul Gem", "Ebony ore", "Dwemer Centurion"],
+        "correct_answer": "Soul Gem"
     },
     {
-        "riddle": "Who wrote 'Romeo and Juliet'?",
-        "options": ["Charles Dickens", "William Shakespeare", "Jane Austen", "Homer"],
-        "correct_answer": "William Shakespeare"
+        "riddle": "A guild of thieves, shadows we trace,\n"
+                  "In the city's embrace, our hidden base.\n"
+                  "Nocturnal's blessing, our path we weave,\n"
+                  "What am I, the cunning rogues believe?\n",
+        "options": ["Thieves Guild", "Dark Brotherhood", "Companion", "College of Winterhold"],
+        "correct_answer": "Thieves Guild"
+    },
+    {
+        "riddle": "Scaled and winged, a terror in flight,\n"
+                  "In Sovngarde's glow, I bring forth fright.\n"
+                  "Dragonborn's bane, with eyes so keen,\n"
+                  "What am I, feared in Skyrim's serene?\n",
+        "options": ["Dragon", "Wyrm", "Draugr", "Frostbite Spider"],
+        "correct_answer": "Dragon"
     }
 ]
